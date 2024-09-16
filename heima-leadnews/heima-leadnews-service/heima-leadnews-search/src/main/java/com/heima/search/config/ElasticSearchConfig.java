40ea5990bcb3360954b5f8b4b6d47f22cee349e4
Myr <myr1839045948@gmail.com>
2024-09-16 12:17:49 +0800

    myr01

---
+++ heima-leadnews/heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/config/ElasticSearchConfig.java
@@ -0,0 +1,44 @@
+package com.heima.search.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "elasticsearch")
+public class ElasticSearchConfig {
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+
+    @Bean
+    public RestHighLevelClient client(){
+        // 设置认证提供者
+        final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        return new RestHighLevelClient(RestClient.builder(
+                        new HttpHost(host, port, "http"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder  httpClientBuilder) {
+                        // 为 HTTP 客户端设置认证
+                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                })
+        );
+    }
+}
